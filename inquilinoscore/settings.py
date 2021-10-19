25d39925fd6f5fa77bf73dd002ab0603439e275b
Luciano <carrenoluciano@gmail.com>
2021-10-19 15:21:13 -0300

    change

--- inquilinoscore/settings.py
+++ inquilinoscore/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hz(m#6oxwn1%eq_u1m^vg3a_kyl-59fx^8nfl^k=@!vnsfj5pf'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
