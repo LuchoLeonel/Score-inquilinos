663442017e25244b93aa43da2441503157eb5216
Luciano <carrenoluciano@gmail.com>
2021-10-19 14:41:49 -0300

    subir-github

--- score/util.py
+++ score/util.py
@@ -4,17 +4,22 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate
 from datetime import datetime
 
+# comparar es un función que compara si hay alguna coincidencia de username, nombre, apellido o dni.
 def comparar(user, name):
+    # Pasamos el name a minuscula y el dni del usuario a integer.
     nombre = name.casefold()
     dni = int(user.dni)
+    # Creamos una lista con los valores que vamos a chequear.
     user = [user.username.casefold(), user.nombre.casefold(), user.apellido.casefold()]
     if nombre in user:
         return True
     if nombre.isnumeric() and int(nombre) == dni:
         return True
+    # Si no hay coincidencia retorna falso.
     return False
 
 
+# relacion_score es una función que si el usuario del perfil le autorizó al usuario logueado que lo califique, te devuelve ese model.
 def relacion_score(usuario_1, usuario_2):
     try:
         permite = PermiteScore.objects.get(propietario=usuario_1, inquilino=usuario_2, active=True)
@@ -22,19 +27,22 @@ def relacion_score(usuario_1, usuario_2):
         permite = False
     return permite
 
-
+# Corroboramos si tienen una relación activa (si el usuario_1 le permitió al usuario_2 puntuarlo pero todavìa no lo hizo)
 def allowToScore(usuario_1, usuario_2):
     try:
         permite = PermiteScore.objects.get(inquilino=usuario_1, propietario=usuario_2, active=True)
     except (ObjectDoesNotExist):
         permite = False
 
+    # Devolvemos un booleano.
     if permite:
         allow = True
     else:
         allow = False
     return allow
 
+
+# Corroboramos la contraparte (si el usuario_2 le permitió al usuario_1 puntuarlo pero todavìa no lo hizo).
 def allowToBeScored(usuario_1, usuario_2):
     try:
         permite_2 = PermiteScore.objects.get(propietario=usuario_1, inquilino=usuario_2, active=True)
@@ -48,6 +56,7 @@ def allowToBeScored(usuario_1, usuario_2):
     return allow
 
 
+# Corroboramos si hay una relación terminada porque el usuario_1 ya puntuó al usuario_2.
 def relacion_terminada(usuario_1, usuario_2):
     try:
         permite = PermiteScore.objects.get(propietario=usuario_1, inquilino=usuario_2, active=False)
@@ -55,6 +64,8 @@ def relacion_terminada(usuario_1, usuario_2):
         permite = False
     return permite
 
+
+# Corroboramos si el usuario_2 ya puntuó al usuario_1 y si es correcto devolvemos el valor.
 def my_score(usuario_1, usuario_2):
     try:
         my_score = Score.objects.get(inquilino=usuario_1, propietario=usuario_2)
@@ -62,6 +73,7 @@ def my_score(usuario_1, usuario_2):
         my_score = False
     return my_score
 
+# Corroboramos la contraparte (Si el usuario_1 ya puntuó al usuario_2) y si es correcto devolvemos el valor.
 def they_score(usuario_1, usuario_2):
     try:
         my_score = Score.objects.get(propietario=usuario_1, inquilino=usuario_2)
@@ -69,6 +81,7 @@ def they_score(usuario_1, usuario_2):
         my_score = False
     return my_score
 
+# Corroboramos si hay Scores y si existen devolvemos los valores.
 def check_scores(usuario):
     try:
         scores = Score.objects.filter(inquilino=usuario)
@@ -76,6 +89,8 @@ def check_scores(usuario):
         scores = False            
     return scores
 
+
+# Calculamos el promedio de los scores de un usuario.
 def amount_scores(scores):
     amount = 0
     for score in scores:
@@ -86,6 +101,8 @@ def amount_scores(scores):
         average = 0
     return int(average)
 
+
+# Renderizamos el perfil del usuario.
 def render_profile(request, usuario, scores, message):
  
     if request.user.is_authenticated:
@@ -109,7 +126,7 @@ def render_profile(request, usuario, scores, message):
         "allow": allow,
         })
 
-
+# Renderizamos la página de registro.
 def render_register(request, message):
     return render(request, "score/register.html", {
         "message": message,
@@ -121,12 +138,14 @@ def render_update(request, usuario, message):
         "message": message
         })
 
+# Renderizamos la página para cambiar el password.
 def render_password(request, usuario, message):
     return render(request, "score/password.html", {
         "usuario": usuario,
         "message": message
         })
 
+# Calculamos el tiempo que hay entre la fecha que empezó el alquiler y la fecha que terminó.
 def calcular_time(desde, hasta):
     tiempo = datetime.strptime(hasta, '%Y-%m-%d') - datetime.strptime(desde, '%Y-%m-%d')
     days = tiempo.days
--- score/views.py
+++ score/views.py
@@ -30,6 +30,7 @@ class NewTaskForm(forms.Form):
 
 # Create your views here.
 def index(request):
+    # Cargamos los usuarios existentes y renderizamos el index.
     usuarios = User.objects.all()
     return render(request, "score/index.html", {
         "usuarios": usuarios,
@@ -37,6 +38,7 @@ def index(request):
     })
 
 def inquilinos(request):
+    # Cargamos unicamente los inquilinos y renderizamos el index.
     inquilinos = User.objects.filter(inquilino=True)
     return render(request, "score/index.html", {
         "usuarios": inquilinos,
@@ -45,13 +47,12 @@ def inquilinos(request):
 
 def login_view(request):
     if request.method == "POST":
-
-        # Attempt to sign user in
+        # Tratamos de loguearnos como usuarios.
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
 
-        # Check if authentication successful
+        # Si se autenticó el usuario cargamos el index, de lo contrario mostramos que hubo un error.
         if user is not None:
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
@@ -65,12 +66,14 @@ def login_view(request):
 
 @login_required(login_url='login')
 def logout_view(request):
+    # Deslogueamos al usuario.
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
 
 def register(request):
     if request.method == "POST":
+        # Cargamos todos los datos a variables.
         username = request.POST["username"]
         nombre = request.POST["nombre"]
         apellido = request.POST["apellido"]
@@ -84,12 +87,13 @@ def register(request):
         inquilino = True if request.POST.get("inquilino") == "si" else False
         propietario = True if request.POST.get("propietario") == "si" else False
 
-        # Ensure password matches confirmation
+        # Nos aseguramos que todo esté bien con el password.
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
+            # Función que cree, está en util.py
             return render_register(request, "Passwords must match.")
- 
+
         if len(password) <= 1:
             return render_register(request, "Passwords must have 8 characters.")
 
@@ -108,14 +112,15 @@ def register(request):
             if int(user["dni"]) == int(dni):
                 return render_register(request, "DNI already registred.")
 
-        # Attempt to create new user
+        # Tratamos de crear un nuevo usuario.
         try:
             user = User.objects.create_user(username = username, dni = dni, nombre = nombre, apellido = apellido,
                 email = email, image = image, password = password, inquilino = inquilino, propietario = propietario)
             user.save()
         except (IntegrityError):
             return render_register(request, "Error.")
-
+        
+        # Logueamos el usuario y redireccionamos al índice.
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
 
@@ -126,6 +131,7 @@ def register(request):
 def update(request):
     user_actual = User.objects.get(id = request.user.id)
     if request.method == "POST":
+        # Cargamos lo datos ingresados.
         username = request.POST["username"]
         nombre = request.POST["nombre"]
         apellido = request.POST["apellido"]
@@ -143,10 +149,12 @@ def update(request):
             if user["email"] == email:
                 return render_update(request, user_actual, "Email already taken.")
         
+        #Cargamos la imagen nueva (si tiene)
         if request.FILES:
             image = request.FILES["adjunto"]
             image.name = username
             user_actual.image = image
+        # Sobreescribimos los valores, guardamos y redireccionamos al perfil.
         user_actual.username = username
         user_actual.nombre = nombre
         user_actual.apellido = apellido
@@ -164,15 +172,20 @@ def update(request):
 def changePassword(request):
     usuario = User.objects.get(id = request.user.id)
     if request.method == "POST":
+        # Cargamos los datos ingresados.
         password = request.POST["password"]
         newPassword = request.POST["newPassword"]
         confirmNewPassowrd = request.POST["confirmNewPassowrd"]
+
+        # Chequeamos que todo este bien con los passwords.
         if not usuario.check_password(password):
             return render_password(request, usuario, "Enter the right password.")
         if newPassword != confirmNewPassowrd:
             return render_password(request, usuario, "Passwords must match.")
         if len(newPassword) <= 1:
             return render_password(request, usuario, "Passwords must have 8 characters.")
+        
+        # Guardamos el nuevo password, logueamos y redireccionamos.
         usuario.set_password(newPassword)
         usuario.save()
         login(request, usuario)
@@ -182,18 +195,25 @@ def changePassword(request):
 
 
 def profile(request, id):
+    # Cargamos el usuario, los scores que otros usuarios hicieron de él y calculamos el average score.
     usuario = User.objects.get(id=id)
+    # check_scores es una función que te devuelve los scores del usuario. Está en util.py
     scores = check_scores(usuario)
+    # average_score es una función que calcula el promedio de todos los scores. Está en util.py
     average_score = amount_scores(scores)
     
     if request.method == "POST":
+        # Cargamos el usuario logueado, y lo datos ingresados.
         user = User.objects.get(id=request.user.id)
         scoring = request.POST["scoring"]
         desde = request.POST["desde"]
         hasta = request.POST["hasta"]
         
+        # relacion_score es una función que si el usuario del perfil le autorizó al usuario logueado que lo califique, te devuelve ese model.
         relacion = relacion_score(user, usuario)
+        # Corroboramos que todo está en orden.
         if not relacion:
+            # render_profile es una función en util.py que renderiza la página del profile.
             return render_profile(request, usuario, scores, "No se pudo guardar, por favor recarga la página.")
 
         if not desde or not hasta:
@@ -202,14 +222,18 @@ def profile(request, id):
         if hasta < desde or datetime.now() < datetime.strptime(request.POST["hasta"], '%Y-%m-%d'):
             return render_profile(request, usuario, scores, "Por favor ingrese una fecha correcta.")
 
+        # calcular_time es una función que calcula cuanto tiempo se vivió el usuario en ese lugar tomando dos parametros (desde, hasta).
         time = calcular_time(desde, hasta)
         comentario = request.POST["comentario"]
+        # Tratamos de crear el score.
         try:
             crear_score = Score(inquilino = usuario, propietario = user, puntaje = scoring,
                 desde = desde, hasta = hasta, time = time, comentario=comentario)
             crear_score.save()
+            # Cambiamos la relación.active a false para que el usuario ya no lo pueda volver a calificar
             relacion.active = False
             relacion.save()
+            # Cargamos todos los scores del usuario y calculamos el nuevo promedio.
             scores = check_scores(usuario)
             average_score = amount_scores(scores)
             usuario.average = average_score
@@ -220,12 +244,16 @@ def profile(request, id):
     return render_profile(request, usuario, scores, False)
 
 def buscar(request):
+    # Cargamos los usuarios.
     users = User.objects.all()
     if request.method == "POST":
         name = request.POST["buscar"]
         lista = set()
+        #Buscamos entre los usuarios si hay alguna coincidencia.
         for user in users:
+            # comparar es un función que compara si hay alguna coincidencia de username, nombre, apellido o dni.
             iguales = comparar(user, name)
+            # Si hay coincidencia se agrega el usuario a la lista vacía que se creó anteriormente.
             if iguales == True:
                 lista.add(user)
         return render(request, "score/index.html", {
@@ -244,12 +272,16 @@ def buscar(request):
 @login_required(login_url='login')
 def allowScore(request, id, id_user):
     if request.method == "POST":
+        #Cargamos el propietario y el inquilino.
         propietario = User.objects.get(id=id)
         inquilino = User.objects.get(id=id_user)
+        # Corroboramos si tienen una relación activa (si no lo puntuó todavia) o tienen una relación terminada (ya lo puntuó).
         activa = allowToScore(propietario, inquilino)
         terminada = relacion_terminada(propietario, inquilino)
+        # Si alguna es correcta retornamos falso porque no puede volver a permitirle que lo puntue.
         if terminada or activa:
             return HttpResponse(False)
+        # Creamos la relación activa que lo va a permitir puntuar y retornamos verdaero.
         crear_permitir_score = PermiteScore(inquilino = inquilino, propietario = propietario, active=True)
         crear_permitir_score.save()
         return HttpResponse(True)
